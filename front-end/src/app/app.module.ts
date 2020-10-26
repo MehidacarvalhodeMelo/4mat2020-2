722d2a9de70594259a242e22bb015e12b576656e
MehidacarvalhodeMelo <mehida.melo@fatec.sp.gov.br>
2020-10-26 14:18:00 +0000

    (26/10)Termino da interface do usuário

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainToolbarComponent } from './ui/main-toolbar/main-toolbar.component';
+import { UiComponent } from './ui/ui.component';
+import { MainMenuComponent } from './ui/main-menu/main-menu.component';
+import { MainFooterComponent } from './ui/main-footer/main-footer.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainToolbarComponent
+    MainToolbarComponent,
+    UiComponent,
+    MainMenuComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
