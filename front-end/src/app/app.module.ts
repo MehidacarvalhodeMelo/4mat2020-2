8b3f2a5263ca35b3adc90519642d17a1e0379d91
MehidacarvalhodeMelo <mehida.melo@fatec.sp.gov.br>
2020-11-16 16:05:59 +0000

    (16/11)Início do componente do formulário

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { MainFooterComponent } from './ui/main-footer/main-footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CursoListComponent } from './curso/curso-list/curso-list.component';
 import { CursoFormComponent } from './curso/curso-form/curso-form.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -29,7 +30,8 @@ import { CursoFormComponent } from './curso/curso-form/curso-form.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
