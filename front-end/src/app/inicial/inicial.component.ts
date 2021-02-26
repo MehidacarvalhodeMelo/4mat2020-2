3c9c0757e8382a3c75e8d3bdd0b11eea46e0933b
Mehida Carvalho <mehida.melo@fatec.sp.gov.br>
2021-02-26 10:53:59 -0300

    25/6 Inserção de imagem

--- front-end/src/app/inicial/inicial.component.ts
+++ front-end/src/app/inicial/inicial.component.ts
@@ -1,11 +1,4 @@
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { Location } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
-import { ProfessorService } from 'src/app/professor/professor.service';
-import { SalaAula1Service } from 'src/app/sala-aula1/sala-aula1.service';
-import { MestreService } from 'src/app/mestre/mestre.service';
 
 @Component({
     selector: 'app-inicial',
@@ -13,5 +6,5 @@ import { MestreService } from 'src/app/mestre/mestre.service';
     styleUrls: ['./inicial.component.scss']
 })
 export class InicialComponent{
-    title: string = 'Essa é a tela inicial'
+    title: string = 'Sistema de Planejamento Online'
 }       
